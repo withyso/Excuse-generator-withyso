594a03fe51e9c3ebf89db9899184361783a2edd7
Adrian <142854436+withyso@users.noreply.github.com>
2023-10-07 03:45:54 +0000

    generador final

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("fish fish fish, javascript is working ");
+  document.querySelector("#the-excuse").innerHTML = randomExcuse();
+};
+
+let randomExcuse = () => {
+  let quien = ["Mi perro", "Mi abuela", "Mi gato", "Mi tortuga"];
+  let accion = ["se comio", "robo", "rompio"];
+  let que = ["el informe", "las llaves", "la tarea"];
+  let cuando = [
+    "en la casa",
+    "saliendo del trabajo",
+    "durante la clase",
+    "en casa de un amigo",
+    "Jugando anoche"
+  ];
+
+  let quienIndex = Math.floor(Math.random() * quien.length);
+  let accionIndex = Math.floor(Math.random() * accion.length);
+  let queIndex = Math.floor(Math.random() * que.length);
+  let cuandoIndex = Math.floor(Math.random() * cuando.length);
+
+  return (
+    quien[quienIndex] +
+    " " +
+    accion[accionIndex] +
+    " " +
+    que[queIndex] +
+    " " +
+    cuando[cuandoIndex]
+  );
 };
